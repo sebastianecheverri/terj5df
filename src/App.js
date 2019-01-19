6082a1f6d8c0a1fd46828e06fb3fe5cec2456c69
Sebastian Echeverri <sebastianecheverri900@gmail.com>
2019-01-18 21:51:51 -0500

    solved

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      guests: [],
+      newGuestName: "",
+      newGuestLast: "",
+    }
+  }
+  updateGuestName (event) {
+    this.setState({
+      newGuestName: event.target.value
+    })
+  }  
+  updateGuestLast (event) {
+    this.setState({
+      newGuestLast: event.target.value
+    })
+  }
+  handleSubmit = (e) =>{
+    var taskId = this.state.guests.length + 1
+    this.setState({
+      guests: this.state.guests.concat({id: taskId, name: this.state.newGuestName, last: this.state.newGuestLast}),
+      newGuestName: "",
+      newGuestLast: "",
+    })
+    e.preventDefault();
+  }
+  
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input type="text" value={this.state.newGuestName} onChange={this.updateGuestName.bind(this)}  className="form-control" name="first-name" />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input type="text" value={this.state.newGuestLast} onChange={this.updateGuestLast.bind(this)} className="form-control" name="last-name" />
               </div>
 
               <div className="action">
@@ -30,9 +58,10 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.guests.map((guest, index) => <tr> <td>{guest.name}</td> <td>{guest.last}</td></tr>)}
               </tbody>
             </table>
+
           </div>
         </div>
       </div>
@@ -41,5 +70,4 @@ class App extends Component {
 }
 
 export default App
-
 
